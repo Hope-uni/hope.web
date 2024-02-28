13522d6817705250645f5ae5927e447400402d3b
MrSamBar <sambarberena@gmail.com>
2024-02-28 15:55:32 -0600

    refactor: variables configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+
+const bucketURL = process.env.NEXT_PUBLIC_HOSTNAME_AZURE_BUCKET;
+
+const remotePatternsArray = bucketURL?.split(',').map((url) => {
+  return {
+    protocol: 'https',
+    hostname: url + '/**',
+  }
+});
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+/*
+ * @type {import('next').NextConfig} 
+ */
+
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    prependData: `@import "app.scss";`
+  },
+  images: {
+    remotePatterns: remotePatternsArray,
+  },
+}
 
 export default nextConfig;
