1cfe64ce7207305dc5b787e87d6abe4e5a699f9c
MrSamBar <sambarberena@gmail.com>
2024-03-11 23:38:10 -0600

    refactor: styling reset and forgot password screen

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,8 @@
-'use client'
-
-import styles from '@/styles/modules/auth.module.scss';
-import Image from 'next/image';
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input } from 'antd';
-import FormItem from "antd/lib/form/FormItem";
+import { LoginForm } from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
 
     return (
-        <Form
-            name="auth_login"
-            id='auth_form_antd'
-            className={styles.auth_form}
-            initialValues={{ remember: true }}
-        >
-            <h1 className={styles.auth_form_title}>Iniciar sesión</h1>
-            <FormItem
-                name="email"
-                className={styles.auth_form_input}
-                rules={[{ required: true, message: 'Please input your Username!' }]}
-            >
-                <Input placeholder="Correo" />
-            </FormItem>
-            <FormItem
-                name="password"
-                className={styles.auth_form_input}
-                rules={[{ required: true, message: 'Please input your Password!' }]}
-            >
-                <Input
-                    type="password"
-                    placeholder="Contraseña"
-                />
-
-            </FormItem>
-
-            <Button type="primary" htmlType="submit" className={styles.auth_form_submit}>
-                Entrar
-            </Button>
-        </Form>
+        <LoginForm />
     );
 }
---
+++ src/components/auth/ForgotPasswordForm.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import styles from '@/styles/modules/auth.module.scss';
+import { Button, Col, Form, Input, Row } from 'antd';
+import FormItem from "antd/lib/form/FormItem";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { HeaderForm } from '@/components/auth/HeaderForm';
+
+export const ForgotPasswordForm = () => {
+    const router = useRouter();
+
+    const handleOnFinish = () => {
+        router.push('/admin');
+    }
+
+    return (
+        <Form
+            name="auth_forgot_password"
+            id='auth_form_antd'
+            className={styles.auth_form}
+            autoComplete="off"
+            layout='vertical'
+            initialValues={{ remember: true }}
+            onFinish={handleOnFinish}
+        >
+            <HeaderForm
+                title='Restablecer contraseña'
+                caption='Ingrese la dirección de correo electrónico asociada con su cuenta y le enviaremos un enlace para restablecer su contraseña'
+            />
+
+            <Form.Item
+                name="email"
+                rules={[
+                    {
+                        type: 'email',
+                        message: 'Correo electrónico debe ser válido',
+                    },
+                    {
+                        required: true,
+                        message: 'Correo electrónico es requerido',
+                    },
+                ]}
+            >
+                <Input placeholder='Correo electronico' />
+            </Form.Item>
+
+
+
+            <Form.Item className={styles.rowCenter}>
+                <br />
+                <Button type="primary" htmlType="submit" className={styles.auth_form_submit}>
+                    Enviar
+                </Button>
+                <div className={styles.auth_form_link_back_row}>
+                    <p className={styles.auth_form_already_have_account}>
+                        Regresa a
+                    </p>
+                    <Link href='/login' className={styles.auth_form_link_back}>
+                        Iniciar sesión
+                    </Link>
+                </div>
+            </Form.Item>
+
+        </Form>
+    );
+}
