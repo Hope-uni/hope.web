f49b8077c30057849e9a3fed5b794ba8e9552fe0
MrSamBar <sambarberena@gmail.com>
2025-02-13 17:20:12 -0600

    refactor: user delete flow

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -29,7 +29,7 @@ export const theme: ThemeConfig = {
       colorPrimary: COLORS.primaryColor,
       colorTextBase: COLORS.black,
       fontSize: 16,
-      controlHeight: 48,
+      controlHeight: 40,
       algorithm: true, // Enable algorithm,
 
       defaultBg: COLORS.white,
