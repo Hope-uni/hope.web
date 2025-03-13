188934eca55759646d24c61b071bbd74125b5b89
MrSamBar <sambarberena@gmail.com>
2025-03-12 22:00:12 -0600

    feat: create and detail activity
    - config files

--- src/app/admin/activities/page.tsx
+++ src/app/admin/activities/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { HeaderContent } from '@/components/layouts/partials/HeaderContent';
 import ActivityIndex from '@/components/activity/list';
-import { RoutesName } from '@/constants/index';
+import ActivityActions from '@/components/activity/list/ActivityActions';
+import { HeaderContent } from '@/components/layouts/partials/HeaderContent';
 import { Space } from 'antd';
 import { useTranslation } from 'react-i18next';
 
@@ -14,8 +14,7 @@ export default function ActivitiesPage() {
         <HeaderContent
           title={t('Activity.index.title')}
           caption={t('Activity.index.caption')}
-          label={t('Activity.index.createButton')}
-          routeLink={RoutesName.user.create}
+          customBtn={<ActivityActions renderMode="add_activity" />}
         />
         <ActivityIndex />
       </Space>
--- src/services/activity/activity.service.ts
+++ src/services/activity/activity.service.ts
@@ -1,6 +1,6 @@
 import { API_HOPE_PROTECTED, defaultPayload } from '@/config';
 import { API } from '@/constants/ApiUrls';
-import { SingleActivity } from '@/models/schema';
+import { DetailActivity, SingleActivity } from '@/models/schema';
 import { API_PAYLOAD, API_RESPONSE, API_SINGLE_RESPONSE } from '@/models/types';
 import { axiosErrorHandler } from '@/utils/axios';
 
@@ -12,6 +12,32 @@ export const ListActivitiesService = async (
       API_RESPONSE<SingleActivity[]>
     >(API.Activity.Index, {
       params: payload.paginate,
+    });
+
+    return response.data;
+  } catch (error) {
+    return axiosErrorHandler(error);
+  }
+};
+
+export const FindActivityByIdService = async (id: number) => {
+  try {
+    const response = await API_HOPE_PROTECTED.get<API_RESPONSE<DetailActivity>>(
+      `${API.Activity.Index}/${id}`,
+    );
+
+    return response.data;
+  } catch (error) {
+    return axiosErrorHandler(error);
+  }
+};
+
+export const CreateActivityService = async (payload: DetailActivity) => {
+  try {
+    const response = await API_HOPE_PROTECTED.post<
+      API_RESPONSE<SingleActivity>
+    >(API.Activity.Index, {
+      ...payload,
     });
 
     return response.data;
