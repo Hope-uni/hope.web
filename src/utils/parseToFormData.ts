f0cef19d3aeba0ccec4b4e8d7c5c928503d91c42
MrSamBar <sambarberena@gmail.com>
2025-05-08 01:58:27 -0600

    fix: deployment

--- src/utils/parseToFormData.ts
+++ src/utils/parseToFormData.ts
@@ -27,7 +27,7 @@ export const ParseToFormData = (payload: Record<string, unknown>): FormData => {
         return;
       }
 
-      if (value instanceof Blob || value instanceof File) {
+      if (value instanceof Blob) {
         form.append(String(key), value);
         return;
       }
