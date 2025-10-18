d0169d966d8e46ef6382edcf581eae6639181027
MrSamBar <sambarberena@gmail.com>
2025-10-18 15:39:19 -0600

    fix: standardize import quotes and formatting in TableContext

--- src/context/Table/TableContext.ts
+++ src/context/Table/TableContext.ts
@@ -1,16 +1,15 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext } from 'react';
 import { initialState } from '@/context/Table/TableReducer';
-import { I_TableState } from "@/models/types/Table";
-
+import { I_TableState } from '@/models/types/Table';
 
 interface TableReducer {
-    state: I_TableState;
-    dispatch: any
+  state: I_TableState<any>;
+  dispatch: any;
 }
 
 export const TableContext = createContext<TableReducer>({
-    state: initialState,
-    dispatch: null
+  state: initialState,
+  dispatch: null,
 });
 
 export const useTable = () => useContext(TableContext);
